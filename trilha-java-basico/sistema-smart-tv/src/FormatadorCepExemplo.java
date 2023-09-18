017fc2eb950740918e30d250821161b0b42b5e3b
RomualdoFranca <romualdo.ads2022@gmail.com>
2023-09-18 20:04:00 -0300

    tratamento de excessao 2

---
+++ trilha-java-basico/sistema-smart-tv/src/FormatadorCepExemplo.java
@@ -0,0 +1,20 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace(); // esse trecho e substituido
+        }
+        
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+    
+}
